da0861bb0a8b2c291e88d18eb01d6d6a9f9d78ca
Sam Westerman <sam.westerman@usaepay.com>
2020-11-16 17:14:53 -0800

    updated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,11 @@ deserialized into an arbitrary byte stream. Because there's no universal way to
 represent integers (outside of `u8` and `i8`), endian types are provided to
 explicitly denote the endianness when deserializing.
 
-//! This is _different_ from ser/de in that we don't have a self-described format,
-//! but rather an arbitrary sequence of input bytes.
+# Serde
+This is not the same as [serde](https://docs.serde.rs/serde/)! Serde is used to
+serialize/deserialize types regardless of the data format. The [`ReadFrom`]/
+[`WriteTo`] traits are intended to be used at a lower-level, where details such
+as the ordering of bytes is important.
 */
 
 use std::io::{self, Read, Write};
